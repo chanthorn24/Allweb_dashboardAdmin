d048a3bfe51b252c6ec372bccde97fd321f7f81b
Thorn324 <lychanthorn2000@gmail.com>
2022-08-12 14:28:42 +0700

    dashboard 12-Aug-2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttendanceComponent } from './component/attendance/attendance.component';
+import { ChangePasswordComponent } from './component/change-password/change-password.component';
+import { EmployeeLeaveComponent } from './component/employee-leave/employee-leave.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { LeaveTypeComponent } from './component/leave-type/leave-type.component';
+import { ReportComponent } from './component/report/report.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: DashboardComponent},
+  {path: "employee", component: EmployeeComponent},
+  {path: "attendance", component: AttendanceComponent},
+  {path: "report", component: ReportComponent},
+  {path: "leave type", component: LeaveTypeComponent},
+  {path: "change password", component: ChangePasswordComponent},
+  {path: "employee/leave", component: EmployeeLeaveComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { AttendanceComponent } from './component/attendance/attendance.component';
+import { ReportComponent } from './component/report/report.component';
+import { LeaveTypeComponent } from './component/leave-type/leave-type.component';
+import { ChangePasswordComponent } from './component/change-password/change-password.component';
+import { EmployeeLeaveComponent } from './component/employee-leave/employee-leave.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    AttendanceComponent,
+    ReportComponent,
+    LeaveTypeComponent,
+    ChangePasswordComponent,
+    EmployeeLeaveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]

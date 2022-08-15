7772ac9e48fdbb8272e5daa0898346baf43a2149
sophea <dunno@gmail.com>
2022-08-15 11:09:35 +0700

    fixed forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,16 +13,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  {path: "login", component: LoginComponent},
-  {path: "forgot password", component: ForgotPasswordComponent, canActivate: [AuthGuard]},
-  {path: "", component: DashboardComponent, canActivate: [AuthGuard]},
-  {path: "employee", component: EmployeeComponent, canActivate: [AuthGuard]},
-  {path: "employee/detail/:id", component: EmployeeDetailComponent, canActivate: [AuthGuard]},
-  {path: "attendance", component: AttendanceComponent, canActivate: [AuthGuard]},
-  {path: "report", component: ReportComponent, canActivate: [AuthGuard]},
-  {path: "leave type", component: LeaveTypeComponent, canActivate: [AuthGuard]},
-  {path: "change password", component: ChangePasswordComponent, canActivate: [AuthGuard]},
-  {path: "employee/leave", component: EmployeeLeaveComponent, canActivate: [AuthGuard]},
+  { path: "login", component: LoginComponent },
+  { path: "forgot password", component: ForgotPasswordComponent },
+  { path: "", component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: "employee", component: EmployeeComponent, canActivate: [AuthGuard] },
+  { path: "employee/detail/:id", component: EmployeeDetailComponent, canActivate: [AuthGuard] },
+  { path: "attendance", component: AttendanceComponent, canActivate: [AuthGuard] },
+  { path: "report", component: ReportComponent, canActivate: [AuthGuard] },
+  { path: "leave type", component: LeaveTypeComponent, canActivate: [AuthGuard] },
+  { path: "change password", component: ChangePasswordComponent, canActivate: [AuthGuard] },
+  { path: "employee/leave", component: EmployeeLeaveComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({

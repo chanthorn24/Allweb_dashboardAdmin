dcf480cfd36cd54704a236af7bc675f854944d3d
Thorn324 <lychanthorn2000@gmail.com>
2022-08-24 14:06:08 +0700

    fix auth

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,11 +1,16 @@
   import { Injectable } from '@angular/core';
   import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
   import { Location } from '@angular/common';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
      providedIn: 'root'
   })
   export class AuthGuard implements CanActivate {
-    constructor(private router: Router, private location: Location) { }
+    constructor(
+      private router: Router,
+      private location: Location,
+      private jwtHelper: JwtHelperService
+      ) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
       // if (this.location.path() == "/login" && this.isLoggedIn()) {
@@ -37,14 +42,14 @@
   }
   public isAdmin(): boolean {
     let status = false;
-    let user = "USER_ROLE";
-    if (user === "ADMIN_ROLE") {
-        status = true;
-      }
-      else {
-        status = false;
-      }
-     return status;
+    let user = this.getRole();
+    if (user === "ADMIN_ROLE" || user === "RECRUITER_ROLE") {
+      status = true;
+    }
+    else {
+      status = false;
+    }
+    return status;
   }
 
   public autoLogOut(timeExpire: number) {
@@ -56,5 +61,12 @@
   public logout() {
     localStorage.removeItem("token");
     this.router.navigateByUrl("/login");
+  }
+
+  public getRole(): string {
+    let token = window.sessionStorage.getItem('token');
+    let tokenData = this.jwtHelper.decodeToken(token ?? "");
+
+    return tokenData.roles.role;
   }
 }
